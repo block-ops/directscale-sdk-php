a1ff34f6221f2b588b0417b8e20da92f900efaaa
rasclatt <rasclatt@me.com>
2020-02-18 16:13:29 -0800

    Core updates

--- src/Model.php
+++ src/Model.php
@@ -6,30 +6,43 @@ namespace DirectScale;
 class Model extends \Nubersoft\nApp
 {
 	protected	static	$con;
-	private	$endpoint_dev	=	'dsapi-dev';
+	private		static	$url		=	false;
+	private		static	$version	=	'v1';
 	/**
 	 *	@description	
 	 */
-	public	function __construct($type = 'dev')
+	public	function __construct($env = false)
 	{
-		$url	=	'https://';
-		switch($type){
-			case('dev'):
-				$url	.=	$this->endpoint_dev;
-				break;
-		}
-		$url		.=	'.directscale.com/v1/';
-		self::$con	=	new \Nubersoft\nRemote($url, false);
+		self::setUrl($env);
+		self::$con	=	new \Nubersoft\nRemote(self::$url, false);
 		# Create a define() for the API in your application key like:
 		# define('DIRECTSCALE_APIKEY', 'thekey123goeshere321');
-		self::$con->addHeader('Ocp-Apim-Subscription-Key', DIRECTSCALE_APIKEY);
+		self::$con->addHeader(...[
+			'Ocp-Apim-Subscription-Key',
+			constant("DIRECTSCALE_".strtoupper($env)."APIKEY")
+		]);
+	}
+	/**
+	 *	@description	
+	 */
+	public	static function setUrl($env)
+	{	
+		$type		=	(!empty($env))? "-".strtolower($env) : $env;
+		self::$url	=	"https://dsapi{$type}.directscale.com/".self::$version."/";
+	}
+	/**
+	 *	@description	
+	 */
+	public	static function setVersion($version)
+	{
+		self::$version	=	$version;
 	}
 	/**
 	 *	@description	
 	 */
 	public	function doService($path, $attr = false, $type = 'get', $func = false)
 	{
-		$data	=	self::$con->doService($path)->query($attr, false, $type)->getResults(false);
+		$data	=	self::$con->{__FUNCTION__}($path)->query($attr, false, $type)->getResults(false);
 		
 		return (is_callable($func))? $func($data) : $data;
 	}
@@ -67,5 +80,37 @@ class Model extends \Nubersoft\nApp
 	public	function doPatch($path, $attr = false, $func = false)
 	{
 		return $this->doService($path, $attr, 'patch', $func);
+	}
+	/**
+	 *	@description	
+	 */
+	public	function getEndpoint()
+	{
+		return $this->url;
+	}
+	/**
+	 *	@description	
+	 */
+	public	function normalizeKeys($array)
+	{
+		$new	=	[];
+		foreach($array as $key => $value) {
+			$vkey	=	ltrim(implode('_', array_map('strtolower', preg_split('/(?=[A-Z])/', $key))), '_');
+			if(!is_array($value)) {
+				$new[$vkey]	=	$value;
+			}
+			else {
+				$new[$vkey]	=	$this->normalizeKeys($value);
+			}
+		}
+		
+		return $new;
+	}
+	/**
+	 *	@description	
+	 */
+	public	function getResourceFile($file)
+	{
+		return (is_file($file = realpath(__DIR__.DS.'..'.DS.'resources').DS.ltrim($file, DS)))? $file : false;
 	}
 }
--- src/User.php
+++ src/User.php
@@ -5,7 +5,7 @@ namespace DirectScale;
  */
 class User extends Model
 {
-	protected	$distid;
+	protected	$distid, $data;
 	/**
 	 *	@description	
 	 */
@@ -20,10 +20,82 @@ class User extends Model
 		parent::__construct();
 	}
 	/**
+	 *	@description	Fetches distributor information based on their distributor ID (rather than primary key)
+	 *	@param	$reformat	[bool]	Default set to reformat the returned data to more compatable keys and data grouping
+	 */
+	public	function getDistInfo($reformat = true)
+	{
+		$this->data	=
+		$data		=	$this->doGet('customers/?backofficeid='.$this->distid);
+		
+		if(empty($data))
+			return $data;
+		
+		if(!$reformat)
+			return $this->getData();
+		
+		$Conv	=	$this->getHelper('Conversion\Data');
+		$data	=	$this->normalizeKeys(json_decode($data, 1));
+		$map	=	$Conv->xmlToArray($this->getResourceFile('gsmapping'.DS.'getdistinfo.xml'));
+		$bill	=	$Conv->xmlToArray($this->getResourceFile('gsmapping'.DS.'billing.xml'));
+		
+		$shipping	=	\Nubersoft\ArrayWorks::interChangeArrays(array_merge(['first_name' => $data[0]['first_name'], 'last_name' => $data[0]['last_name']], $data[0]['default_shipping_address']), $bill);
+		$billing	=	\Nubersoft\ArrayWorks::interChangeArrays(array_merge(['first_name' => $data[0]['first_name'], 'last_name' => $data[0]['last_name']], $data[0]['primary_address']), $bill);
+		
+		unset($data[0]['primary_address'], $data[0]['default_shipping_address']);
+		
+		$main		=	$this->sortUserData(\Nubersoft\ArrayWorks::interChangeArrays($data[0], $map));
+		
+		$main['billing']	=	$billing;
+		$main['shipping']	=	$shipping;
+		
+		return $main;
+	}
+	
+	public	function sortUserData($array=false)
+	{
+		$new	=	[];
+		$array	=	(is_array($array))? $array : $this->raw_user;
+			
+		foreach($array as $key => $value) {
+			if(preg_match('/^user/', $key) || in_array($key, ['internal_id','join_date','highest_achieved_rank','current_rank','avatar','first_name','last_name','full_name','member_type','email_address','night_phone','cell_phone'])) {
+				if($key == 'internal_id')
+					$new['user']['distid']	=	$value;
+				else
+					$new['user'][$key]	=	$value;
+			}
+			elseif(preg_match('/^billing_/', $key)) {
+				$new['billing'][str_replace('billing_','',$key)]	=	$value;
+			}
+			elseif(preg_match('/^shipping_/', $key)) {
+				$new['shipping'][str_replace('shipping_','',$key)]	=	$value;
+			}
+			elseif(preg_match('/^cc_/', $key) || preg_match('/^exp_/', $key) || preg_match('/_card/', $key)) {
+				$new['credit_card'][str_replace(['cc_', 'billing_'],'',$key)]	=	$value;
+			}
+			elseif(preg_match('/^autoship_/', $key)) {
+				
+				$new['autoship']	=	true;
+			}/*
+			elseif(preg_match('/_qv/', $key)) {
+				if(stripos($key, 'left') !== false)
+					$new['volume']['leg_left'][$key]	=	$value;
+				else
+					$new['volume']['leg_right'][$key]	=	$value;
+			}*/
+			else
+				$new['general'][$key]	=	$value;
+		}
+		
+		ksort($new);
+		
+		return $new;
+	}
+	/**
 	 *	@description	
 	 */
-	public	function getDistInfo()
+	public	function getData()
 	{
-		return $this->doGet('customers/'.$this->distid);
+		return $this->data;
 	}
 }
