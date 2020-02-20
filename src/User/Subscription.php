d1b0020bcacf3751769cdf14cae05cface2bbaae
rasclatt <rasclatt@me.com>
2020-02-20 15:02:55 -0800

    Added Autoship Deletion
    Not tested as of this version

--- src/User/Subscription.php
+++ src/User/Subscription.php
@@ -28,4 +28,35 @@ class Subscription
 	{
 		return $this->autoships;
 	}
+	/**
+	 *	@description	
+	 */
+	public	function delete($id = false)
+	{
+		$count	=	count($this->autoships);
+		if($count == 1) {
+			# Delete
+			$this->doDelete('orders/autoship/'.$this->autoships[0]['id']);
+			# Reset the autoships
+			$this->__construct($this->User);
+		}
+		elseif($count > 1) {
+			foreach($this->autoships as $key => $as) {
+				# See if id is supplied and matches
+				$idmatch	=	(!empty($id) && ($as['id'] == $id));
+				# See if an id match OR there is no specific id to delete
+				if($idmatch || empty($id)) {
+					# Delete the autoship if set
+					$this->doDelete('orders/autoship/'.$as['id']);
+					# Stop if an id is supplied
+					if(!empty($id))
+						break;
+				}
+			}
+			# Reset the autoships
+			$this->__construct($this->User);
+		}
+		
+		return $this->autoships;
+	}
 }
